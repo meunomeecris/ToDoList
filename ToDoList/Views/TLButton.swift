973bbcdf3ce85d8d683893ad0b72221dd37e1f8d
Cris Messias <meunomeecriss@gmail.com>
2023-07-02 10:53:12 +0200

    :art: Improve structure / format of the code.
    Creating a view for buttons

---
+++ ToDoList/Views/TLButton.swift
@@ -0,0 +1,40 @@
+//
+//  TLButton.swift
+//  ToDoList
+//
+//  Created by Cris Messias on 02/07/23.
+//
+
+import SwiftUI
+
+struct TLButton: View {
+    let title: String
+    let backgroundColor: Color
+    let foregroundColor: Color
+    let action: () -> Void
+    
+    var body: some View {
+        Button {
+            //Action
+            action()
+        } label: {
+            ZStack{
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(backgroundColor)
+                    .frame(width: 300, height: 50)
+                
+                Text(title)
+                    .font(.system(size: 18))
+                    .foregroundColor(foregroundColor)
+            }
+        }
+    }
+}
+
+struct TLButton_Previews: PreviewProvider {
+    static var previews: some View {
+        TLButton(title: "Button", backgroundColor: .black, foregroundColor: .white) {
+            //action
+        }
+    }
+}
