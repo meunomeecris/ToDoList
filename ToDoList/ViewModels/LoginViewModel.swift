cdb3cd5567bf7398dc40e12e863b15e3147ff669
Cris Messias <meunomeecriss@gmail.com>
2023-07-02 10:53:29 +0200

    🎨 Improve structure / format of the code.

--- ToDoList/ViewModels/LoginViewModel.swift
+++ ToDoList/ViewModels/LoginViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class LoginViewModel: ObservableObject {
+    @Published var email: String = ""
+    @Published var password: String = ""
+    
+    init() {
+        
+    }
+    
+    func login() {
+        
+    }
+    
+    func validateLogin() {
+        
+    }
+}
--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var name: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack{
+            //Header
+            HeaderView(title: "Register", subtitle: "Start organizing ToDO's!", angle: -8, backgroundColor: .teal)
+            
+            Spacer()
+            
+            //Form
+            Form{
+                TextField("Full Name", text: $name)
+                    .autocorrectionDisabled()
+                TextField("Email Adress", text: $email)
+                    .textInputAutocapitalization(.never)
+                    .autocorrectionDisabled()
+                SecureField("Password", text: $password)
+                
+                TLButton(
+                    title: "Create",
+                    backgroundColor: .teal,
+                    foregroundColor: .white)
+                {
+                    //action
+                }
+            }
+            .offset(y: -5)
+        } //VStack
+    } //body
+} //view
 
 struct RegisterView_Previews: PreviewProvider {
     static var previews: some View {
