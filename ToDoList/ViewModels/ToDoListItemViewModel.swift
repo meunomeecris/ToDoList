66dd9f1f6463e8f01cd3001225a7edac470f0cae
Cris Messias <meunomeecriss@gmail.com>
2023-07-07 11:16:32 +0200

    Update ToDoListItemViewModel.swift

--- ToDoList/ViewModels/ToDoListItemViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewModel.swift
@@ -5,10 +5,29 @@
 //  Created by Cris Messias on 30/06/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 //ViewModel for "single" Item
 class ToDoListItemViewModel: ObservableObject {
     
     init() {}
+    
+    func toggleIsDone(item: ToDoListItemModel) {
+        var newItem = item
+        newItem.setDone(!item.isDone)
+        
+        //update in datebase
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let database = Firestore.firestore()
+        database.collection("users")
+            .document(uid)
+            .collection("toDos")
+            .document(newItem.id)
+            .setData(newItem.asDictionary())
+    }
 }
