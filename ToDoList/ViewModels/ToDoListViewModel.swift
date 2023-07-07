68dd84b5e463761a3f0ed6b198ae02a3b83f73c0
Cris Messias <meunomeecriss@gmail.com>
2023-07-07 11:16:35 +0200

    Update ToDoListViewModel.swift

--- ToDoList/ViewModels/ToDoListViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewModel.swift
@@ -5,10 +5,27 @@
 //  Created by Cris Messias on 30/06/23.
 //
 
+import FirebaseFirestore
 import Foundation
 
 class ToDoListViewModel: ObservableObject {
     @Published var showingNewItemView = false
+    private let userId: String
     
-    init() {}
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    
+    /// Delete to do  list item
+    /// - Parameter id: item id to delete
+    func delete(id: String) {
+        let database = Firestore.firestore()
+        
+        database.collection("users")
+            .document(userId)
+            .collection("toDos")
+            .document(id)
+            .delete()
+    }
 }
